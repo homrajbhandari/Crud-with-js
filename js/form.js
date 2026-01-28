1226862d4e82204488d8f3f9883c6f8c5c4d0c83
Hom Raj Bhandari <homrajbhandari96@gmail.com>
2026-01-28 07:46:54 +0545

    updated form.js for EditItem

--- js/form.js
+++ js/form.js
@@ -1,15 +1,24 @@
-import {addItem} from "./app.js";
+import {addItem,updateItemName} from "./app.js";
 
-export function createForm()
+export function createForm(editId, itemToEdit)
 {
     const form=document.createElement("form");
 
-    form.innerHTML=
-    `<h2>Grocery bud</h2>
-          <div class="form-control">
-            <input type="text" class="form-input" placeholder="e.g. eggs">
-            <button type="submit" class="btn">Add Item</button>
-          </div>`;
+   form.innerHTML = `
+    <h2>grocery bud</h2>
+    <div class="form-control">
+      <input
+        type="text"
+        class="form-input"
+        placeholder="e.g. eggs"
+        value="${itemToEdit ? itemToEdit.name : ""}"
+      />
+      <button type="submit" class="btn">
+        ${editId ? "edit item" : "add item"}
+      </button>
+    </div>
+  `;
+
 
           form.addEventListener("submit",(e)=>{
             e.preventDefault();
@@ -17,12 +26,18 @@ export function createForm()
             const value=input.value.trim();
             if(!value)
             {
-                alert ("Please provide value");
+                alert ("Please provide value","error");
                 return;
             }
 
-            addItem(value);
-            input.value="";
-          });
-          return form;
+            if (editId) {
+      updateItemName(value);
+    } else {
+      addItem(value);
+    }
+
+    input.value = "";
+  });
+
+  return form;
 }
